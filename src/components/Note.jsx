8709624b5d850f25d6c93784eedc17c863b77923
IkramNizamani <63128235+IkramNizamani@users.noreply.github.com>
2021-02-21 15:08:39 +0500

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Note(){
+    return(
+        <div className='note'>
+            <h1>This is the title for note.</h1>
+            <p>This is the content for the note.</p>
+        </div>
+    );
+}
+export default Note;
